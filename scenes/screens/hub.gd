3f2e3fd1a939f72b4fd4f011822c60b39fd1a8ae
HasanMafatihul <16521045@mahasiswa.itb.ac.id>
2021-11-18 12:31:34 +0700

    Added functional hub. Exported build-1. Todo: death screen.
    Signed-off-by: HasanMafatihul <16521045@mahasiswa.itb.ac.id>

---
+++ scenes/screens/hub.gd
@@ -0,0 +1,29 @@
+extends Control
+
+export var item = {
+	"Pen": [null, "Just a pen", 100],
+	"You": [null, "You(?)", 150],
+	"Cheap": ["res://icon.png", "Cheap item", 50]
+}
+
+var item_name = []
+var selected
+
+func _ready():
+	for i in item:
+		$Items.add_item(i)
+		item_name.append(i)
+
+func _process(_delta):
+	$coins.text = "Coins : " + str(global.coins)
+
+func _on_Items_item_selected(index):
+	selected = item_name[index]
+	if item[selected][0]:
+		$Panel/img.texture = load(item[selected][0])
+	else:
+		$Panel/img.texture = null
+	$Panel/name.text = selected
+	$Panel/desc.text = item[selected][1]
+	$Panel/price.text = str(item[selected][2])
+	
