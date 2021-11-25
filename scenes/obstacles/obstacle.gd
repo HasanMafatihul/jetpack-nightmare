57c019c7b657ec0459ed53387fc8290017887d37
HasanMafatihul <16521045@mahasiswa.itb.ac.id>
2021-11-25 21:55:43 +0700

    Updated obstacles and coins graphic
    Signed-off-by: HasanMafatihul <16521045@mahasiswa.itb.ac.id>

--- scenes/obstacles/obstacle.gd
+++ scenes/obstacles/obstacle.gd
@@ -1,11 +1,19 @@
 extends Area2D
 
+var rng = RandomNumberGenerator.new()
+
 # Obstacle damage. Adjustable
 export var damage = 10
 
+# Obstacle skins
+var skins = [preload("res://visual/Obstacle/Obstacle 1.PNG"), preload("res://visual/Obstacle/Obstacle 2.PNG")]
+
+func _ready():
+	rng.randomize()
+	$sprite.texture = skins[rng.randi_range(0,1)]
+	rotate(rng.randf_range(0,360))
+
 # Obstacle hit something
-
-
 func _on_obstacle_body_entered(body):
 	var layer = body.collision_layer
 	match layer:
